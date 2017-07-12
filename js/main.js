7cf57c1a118e28ac4efcaa7553ab494dbe085e12
Marta Bermejo <m.berlo8583@gmail.com>
2017-07-12 14:52:32 +0200

    change color, added js to add arrow, slow movement to each section

--- js/main.js
+++ js/main.js
@@ -47,13 +47,53 @@ function indicateBlock(elementId) {
 
 function isScrolledIntoView(elementId)
 {
+    var docViewHeight = $(window).height();
     var docViewTop = $(window).scrollTop() * 1.15;
-    var docViewBottom = (docViewTop + $(window).height()) * 0.95;
+    var docViewBottom = (docViewTop + docViewHeight) * 0.95;
 
+    var elementHeight = $('#' + elementId).height();
     var elemTop = $('#' + elementId).offset().top;
-    var elemBottom = (elemTop + $('#' + elementId).height());
+    var elemBottom = (elemTop + elementHeight);
 
-    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop) || (elemBottom >= docViewBottom && elemTop <= docViewTop && elementHeight >= docViewHeight) );
 }
 
 window.addEventListener("scroll", handleScroll);
+
+$(function() {
+
+  $('a.link-nav').click(function() {
+
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+      location.hostname == this.hostname) {
+
+      var $target = $(this.hash);
+
+      $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body').animate({
+          scrollTop: targetOffset
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+$(document).ready(function() {
+  $('.up-page').click(function() {
+    $('body, html').animate({
+      scrollTop: '0px'
+    }, 1000);
+  });
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 0) {
+      $('.up-page').fadeIn(300);
+    } else {
+      $('.up-page').fadeOut(300);
+    }
+  });
+})
